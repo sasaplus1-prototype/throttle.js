4c9fa20ee513a8c712650a655d11ff74bb5bc58d
sasaplus1 <sasaplus1@gmail.com>
2016-07-08 19:16:56 +0900

    added index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const now = (typeof Date.now !== 'undefined') ?
+  function() {
+    return Date.now();
+  } :
+  function() {
+    return new Date().getTime();
+  };
+
+module.exports = function(fn, interval) {
+  var last = now() - interval;
+
+  return function() {
+    if (last + interval <= now()) {
+      last = now();
+
+      fn.apply(this, arguments);
+    }
+  };
+};
