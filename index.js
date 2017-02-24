937277ee1954b5ab322b9db73d76d36f2a25ad5e
sasaplus1 <sasaplus1@gmail.com>
2017-02-24 16:09:40 +0900

    improved

--- index.js
+++ index.js
@@ -5,17 +5,33 @@ var now = (typeof Date.now !== 'undefined') ?
     return Date.now();
   } :
   function() {
-    return new Date().getTime();
+    return (new Date).getTime();
   };
 
 module.exports = function(fn, interval) {
   var last = now() - interval;
 
   return function() {
-    if (last + interval <= now()) {
-      last = now();
+    var args;
 
-      fn.apply(this, arguments);
+    if (last + interval > now()) {
+      return;
+    }
+
+    last = now();
+    args = arguments;
+
+    switch (args.length) {
+      case 0:
+        return fn.call(this);
+      case 1:
+        return fn.call(this, args[0]);
+      case 2:
+        return fn.call(this, args[0], args[1]);
+      case 3:
+        return fn.call(this, args[0], args[1], args[2]);
+      default:
+        return fn.apply(this, args);
     }
   };
 };
